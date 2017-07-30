fc821a3ec47afe5f901e763b64e87a11ea5959e0
drw <drw@users.noreply.github.com>
2017-07-30 07:54:13 -0400

    Fix handling of resource-only queries and group quoting and add timestamp support

--- urls.py
+++ urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
-    url(r'^(?P<resource_id>[^/]+)/(?P<query_string>.+)/csv$', views.query_csv_view, name='query_csv_view'), 
-    url(r'^(?P<resource_id>[^/]+)/(?P<query_string>.+)/$', views.parse_and_query, name='parse_and_query'), 
+    url(r'^(?P<resource_id>[^/]+)/(?P<query_string>.*/|)csv$', views.query_csv_view, name='query_csv_view'), 
+    url(r'^(?P<resource_id>[^/]+)/(?P<query_string>.*/|)$', views.parse_and_query, name='parse_and_query'), 
     url(r'^(?P<resource_id>[^/]+)/(?P<field>.*)/(?P<search_term>.*)/csv$', views.csv_view, name='show_csv'),
     url(r'^(?P<resource_id>[^/]+)/(?P<field>.*)/(?P<search_term>.*)$', views.results, name='results'),
     ]
--- views.py
+++ views.py
@@ -6,6 +6,7 @@ import requests
 from json2html import *
 import ckanapi
 from pprint import pprint
+from datetime import datetime
 
 import re, csv, sys
 try:
@@ -18,8 +19,11 @@ except:
 # [ ] All CKAN API requests should be API-key-free to avoid any possibility of tables 
 # being dropped or data being modified.
 
-# [ ] Add checkbox to drop duplicate rows and implement by changing SELECT to SELECT DISTINCT.
+# [ ] Implement timestamp support
 
+# Front-end stuff
+# [ ] Implement GUI (probably using JQuery Query Builder, though also consider django-report-builder)
+# [ ] Add checkbox to drop duplicate rows and implement by changing SELECT to SELECT DISTINCT.
 
 DEFAULT_SITE = "https://data.wprdc.org"
 
@@ -174,122 +178,146 @@ def convert_operator(op):
         return '>'
     raise ValueError("{} is an operator for which there is not yet a conversion".format(op))
 
-def generate_query(resource_id,schema,query_string):
-    elements = query_string.split('/')
+def generate_query(resource_id,schema,query_string=''):
     filter_strings = []
     groupbys = []
     aggregators = []
     orderbys = []
-    op = None
-    for e in elements:
-        q_list = e.split('--')
-        # Possible formats: 
-        #    /field1--value1/field2--value2/ (where equality is implicit)
-        #   /field1--eq--value1/field2--lt--value2/groupby--field3/aggregateby--sum/
-        #       (equality (and other relations) are explicit)
-        if len(q_list) == 3: # It's a filter
-            if q_list[0] == 'aggregateby':
-                agg = q_list[1].upper()
-                if agg in ['SUM', 'AVG', 'MIN', 'MAX', 'COUNT']:
-                    aggregators.append("{}({})".format(q_list[1],q_list[2])) 
-                else:
-                    raise ValueError('Unknown aggregator function {} found'.format(q_list[1]))
-                # In the URL, aggregators are of the form
-                # /aggregateby--sum--field_name/
-                # and then they become functions in the 
-                # aggregators list, such that aggregators looks like
-                #   aggregators = ['SUM(field_name)','AVG(whoa)']
-            elif q_list[0] == 'orderby':
-                field = q_list[1]
-                direction = q_list[2].upper()
-                if direction in ['ASC','DESC','']:
-                    orderbys.append("{} {}".format(field,direction)) 
-                else:
-                    raise ValueError('Unknown ordering direction {} found'.format(q_list[2]))
-            else: 
-                #r = query_resource(site,  'SELECT * FROM "{}" WHERE venue_type = \'Church\' LIMIT 3'.format(resource_id), API_key)
-                # Knowing the types of the fields is important for formatting the query
-                schema_types = schema_dict(schema)
-                field_type = schema_types[q_list[0]]
-                if field_type in ['numeric','float8','int4','int8']:
-                    filter_s = '"{}" {} {}'.format(q_list[0], convert_operator(q_list[1]), q_list[2])
-                elif field_type in ['text','JSON','json']: 
-                    op = convert_operator(q_list[1])
-                    filter_s = '"{}" {} '.format(q_list[0], op)
-                    if op == 'LIKE':
-                        filter_s += "'%{}%'".format(q_list[2])
+    if query_string != '':
+        elements = query_string.split('/')
+        op = None
+        for e in elements:
+            q_list = e.split('--')
+            # Possible formats: 
+            #    /field1--value1/field2--value2/ (where equality is implicit)
+            #   /field1--eq--value1/field2--lt--value2/groupby--field3/aggregateby--sum--field4/
+            #       (equality (and other relations) are explicit)
+            if q_list == ['']:
+                pass
+            elif len(q_list) == 3: # It's a filter
+                if q_list[0] == 'aggregateby':
+                    agg = q_list[1].upper()
+                    if agg in ['SUM', 'AVG', 'MIN', 'MAX', 'COUNT']:
+                        aggregators.append("{}({})".format(q_list[1],q_list[2])) 
                     else:
-                        filter_s += "'{}'".format(q_list[2])
-                elif field_type in ['bool','boolean']: 
-                    op = convert_operator(q_list[1])
-                    if op in ['!~','!=']:
-                        filter_s = '("{}" != {} OR "{}" IS NULL)'.format(q_list[0],q_list[2],q_list[0])
-                        # This is how boolean fields work in Postgres:
-                        #    SELECT * FROM test1;
-                        #     a |    b
-                        #    ---+---------
-                        #     t | sic est
-                        #     f | non est
+                        raise ValueError('Unknown aggregator function {} found'.format(q_list[1]))
+                    # In the URL, aggregators are of the form
+                    # /aggregateby--sum--field_name/
+                    # and then they become functions in the 
+                    # aggregators list, such that aggregators looks like
+                    #   aggregators = ['SUM(field_name)','AVG(whoa)']
 
-                        #    SELECT * FROM test1 WHERE a;
-                        #     a |    b
-                        #    ---+---------
-                        #     t | sic est
-                        # https://www.postgresql.org/docs/9.1/static/datatype-boolean.html
+                    # Since parentheses are allowed, this could be changed to 
+                    # /sum(field_name)/
+                    # The available symbols, which don't get URL-encoded, are $-_.+!*'(),
+                    # Thus, even my squiggle is a little potentially problematic.
 
-                        # CREATE TABLE test1 (a boolean, b text);
-                        # INSERT INTO test1 VALUES (TRUE, 'sic est');
-                        # INSERT INTO test1 VALUES (FALSE, 'non est');
-                        # INSERT INTO test1 VALUES (NULL, 'nemo');
-                        # SELECT * FROM test1 WHERE NOT a;
-                        # |     a |       b |
-                        # |-------|---------|
-                        # | false | non est |
-
-                        # SELECT * FROM test1 WHERE a IS NULL; (Null is not a value and 
-                        # can only be detected this way).
-                        #        |      a |    b |
-                        #        |--------|------|
-                        #        | (null) | nemo |
-                        
-                        # So two operators are desirable: x = False and x != True (where x != True
-                        # means x = True OR x IS NULL.
-
-                        # Maybe we can get away without implementing ORs by noting that we can
-                        # always AND together negated filters and then negate the output?
-                        # But then we need a way of negating the overall filter.
+                elif q_list[0] == 'orderby':
+                    field = q_list[1]
+                    direction = q_list[2].upper()
+                    if direction in ['ASC','DESC','']:
+                        orderbys.append("{} {}".format(field,direction)) 
                     else:
+                        raise ValueError('Unknown ordering direction {} found'.format(q_list[2]))
+                else: 
+                    #r = query_resource(site,  'SELECT * FROM "{}" WHERE venue_type = \'Church\' LIMIT 3'.format(resource_id), API_key)
+                    # Knowing the types of the fields is important for formatting the query
+                    schema_types = schema_dict(schema)
+                    field_type = schema_types[q_list[0]]
+                    if field_type in ['numeric','float8','int4','int8']:
                         filter_s = '"{}" {} {}'.format(q_list[0], convert_operator(q_list[1]), q_list[2])
-                else:
-                    raise ValueError("Modify generate_query to handle fields of type {}".format(field_type))
-                # timestamp, text, bool or boolean | [ ] Some of these others need better handling and/or conversion.
-                # I've also seen 'tsvector' (which is used for the _full_text field returned by SQL query requests
-                # and 'nested' though I don't know if the latter is official.
+                    elif field_type in ['text','JSON','json']: 
+                        op = convert_operator(q_list[1])
+                        filter_s = '"{}" {} '.format(q_list[0], op)
+                        if op == 'LIKE':
+                            filter_s += "'%{}%'".format(q_list[2])
+                        else:
+                            filter_s += "'{}'".format(q_list[2])
+                    elif field_type in ['bool','boolean']: 
+                        op = convert_operator(q_list[1])
+                        if op in ['!~','!=']:
+                            filter_s = '("{}" != {} OR "{}" IS NULL)'.format(q_list[0],q_list[2],q_list[0])
+                            # This is how boolean fields work in Postgres:
+                            #    SELECT * FROM test1;
+                            #     a |    b
+                            #    ---+---------
+                            #     t | sic est
+                            #     f | non est
 
-                filter_strings.append(filter_s)
-        elif len(q_list) == 2:
-            if q_list[0] == 'groupby':
-                groupbys.append(q_list[1])
-                # An example of a SQL query with grouping and aggregating:
-                #   SELECT Shippers.ShipperName,COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
-                #        LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
-                #        GROUP BY ShipperName;
+                            #    SELECT * FROM test1 WHERE a;
+                            #     a |    b
+                            #    ---+---------
+                            #     t | sic est
+                            # https://www.postgresql.org/docs/9.1/static/datatype-boolean.html
 
-                # The result is two columns: ShipperName and NumberOfOrders.
+                            # CREATE TABLE test1 (a boolean, b text);
+                            # INSERT INTO test1 VALUES (TRUE, 'sic est');
+                            # INSERT INTO test1 VALUES (FALSE, 'non est');
+                            # INSERT INTO test1 VALUES (NULL, 'nemo');
+                            # SELECT * FROM test1 WHERE NOT a;
+                            # |     a |       b |
+                            # |-------|---------|
+                            # | false | non est |
 
-                # Aggregation goes hand-in-hand with grouping.
-                # Good default aggregations might be number_of_rows and summation of any numeric field.
+                            # SELECT * FROM test1 WHERE a IS NULL; (Null is not a value and 
+                            # can only be detected this way).
+                            #        |      a |    b |
+                            #        |--------|------|
+                            #        | (null) | nemo |
+                            
+                            # So two operators are desirable: x = False and x != True (where x != True
+                            # means x = True OR x IS NULL.
 
-        else: 
-            raise ValueError("q_list is {} elements long, which is an unexpected length".format(len(q_list)))
+                            # Maybe we can get away without implementing ORs by noting that we can
+                            # always AND together negated filters and then negate the output?
+                            # But then we need a way of negating the overall filter.
+                        else:
+                            filter_s = '"{}" {} {}'.format(q_list[0], convert_operator(q_list[1]), q_list[2])
+                    elif field_type in ['date','timestamp']: 
+                        # /start_date--gt--2016-03-01/
+                        # /start_date--gt--2016-03-01-1300/
+                        op = convert_operator(q_list[1])
+                        try:
+                            date_limit = q_list[2]
+                            limiting_date = datetime.strptime(date_limit, "%Y-%m-%d")
+                            filter_s = '"{}" {} '.format(q_list[0], op)
+                            filter_s += "'{}'".format(q_list[2])
+                        except:
+                            datetime_limit = q_list[2]
+                            limiting_dt = datetime.strptime(datetime_limit, "%Y-%m-%d-%H%M")
+                            filter_s = '"{}" {} '.format(q_list[0], op)
+                            limiting_dt_s = datetime.strftime(limiting_dt, '%Y-%m-%d %H:%M:%S')
+                            filter_s += "'{}'".format(limiting_dt_s)
+                    else:
+                        raise ValueError("Modify generate_query to handle fields of type {}".format(field_type))
+                    # timestamp, text, bool or boolean | [ ] Some of these others need better handling and/or conversion.
+                    # I've also seen 'tsvector' (which is used for the _full_text field returned by SQL query requests
+                    # and 'nested' though I don't know if the latter is official.
+
+                    filter_strings.append(filter_s)
+            elif len(q_list) == 2:
+                if q_list[0] == 'groupby':
+                    groupbys.append('"{}"'.format(q_list[1]))
+                    # An example of a SQL query with grouping and aggregating:
+                    #   SELECT Shippers.ShipperName,COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
+                    #        LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+                    #        GROUP BY ShipperName;
+
+                    # The result is two columns: ShipperName and NumberOfOrders.
+
+                    # Aggregation goes hand-in-hand with grouping.
+                    # Good default aggregations might be number_of_rows and summation of any numeric field.
+            else: 
+                raise ValueError("q_list is {} elements long, which is an unexpected length".format(len(q_list)))
 
     query = 'SELECT * FROM "{}"'.format(resource_id)
     if len(groupbys) > 0:
         query = 'SELECT '
         if len(groupbys) > 0:
-            query += "{}, ".format(', '.join(groupbys))
+         query += '{}, '.format(', '.join(groupbys))
         if len(aggregators) > 0:
-            query += "{}, ".format(', '.join(aggregators))
+            for a in aggregators:
+                query += '{}, '.format(a)
         query += 'COUNT("_id") as "count" FROM "{}"'.format(resource_id)   
     
     if len(filter_strings) > 0:
@@ -323,8 +351,6 @@ def query_csv_view(request,resource_id,query_string):
     chunk_size = 3000
     ckan = ckanapi.RemoteCKAN(site)
     written, total = get_and_write_next_rows(ckan,resource_id,query,None,None,writer,chunk_size,offset=0,written=0)
-    # [ ] Maybe refactor get_and_write_next_rows to handle either field/search_term-style queries OR
-    # SQL queries.
 
     while written < total:
         offset = offset+chunk_size
